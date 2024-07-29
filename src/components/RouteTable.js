08c45f92c4daf2687e11f5093059365824b888bd
ShaharAshe <shaharas30@gmail.com>
2024-07-29 20:04:29 +0300

    Add Error page

--- src/components/RouteTable.js
+++ src/components/RouteTable.js
@@ -1,10 +1,11 @@
-import {BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
+import {HashRouter, Route, Routes } from "react-router-dom";
 import React from "react";
 import Home from "./Home";
 import About from "./About";
 import Projects from "./Projects"
 import Contact from "./Contact"
 import Header from "./Header";
+import ErrorPage from "./ErrorPage"
 import '../css/style.css'
 
 
@@ -17,7 +18,7 @@ function RouteTable(){
                 <Route path="/about" element={<About/>}/>
                 <Route path="/projects" element={<Projects/>}/>
                 <Route path="/contact" element={<Contact/>}/>
-                {/* <Route path="*" element={<ErrorPage/>} /> */}
+                <Route path="*" element={<ErrorPage/>} />
             </Routes>
         </HashRouter>
     );
