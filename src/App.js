e2369f267aaafebb7508b546438fb010c5ce0461
ShaharAshe <shaharas30@gmail.com>
2024-09-05 18:21:56 +0300

    Update Google Analytics tracking code and ReactGA initialization

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { useEffect } from 'react';
 import './App.css';
 import RouteTable from './components/RouteTable';
 import './css/style.css'
 
 
 function App() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${process.env.REACT_APP_TRACKING_ID}`;
+    script.async = true;
+    document.head.appendChild(script);
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      window.dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+    gtag('config', process.env.REACT_APP_TRACKING_ID);
+  }, []);
   return (
     <div className="App">
       <RouteTable/>
