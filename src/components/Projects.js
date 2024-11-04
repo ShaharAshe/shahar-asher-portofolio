3f59b25ee166626d606c00c318ec81834225e84b
ShaharAshe <shaharas30@gmail.com>
2024-11-04 10:17:37 +0200

    Add Exchange Rate Converter project and update ProjectsCards styling

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -43,6 +43,11 @@ function Projects() {
                     link="https://github.com/ShaharAshe/projec_euler"
                 />
                 <ProjectsCards
+                    title="Exchange Rate Converter"
+                    languageTools="C#, .NET, API Integration"
+                    link="https://github.com/ShaharAshe/ExchangeRateConverter"
+                />
+                <ProjectsCards
                     title="GPA Calculator"
                     languageTools="Python"
                     link="https://gitlab.com/ShaharAshe/avg_gpa_calculator"
--- src/components/ProjectsCards.js
+++ src/components/ProjectsCards.js
@@ -5,7 +5,7 @@ function ProjectsCards(props) {
         <div className="card project-card">
             <div className="card-body">
                 <h5 className="card-title">{props.title}</h5>
-                <p className="card-text"><strong>Languages and Tools:</strong> {props.languageTools}</p>
+                <p className="card-text"><hr/><strong>Languages and Tools</strong><br/><br/>{props.languageTools}</p>
                 {props.link && (
                     <a href={props.link} target="_blank" rel="noopener noreferrer" className="btn btn-primary">
                         View Project
