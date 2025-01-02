92c136527af85e5e2f6d2ddc8b458acc98e6a01c
ShaharAshe <shaharas30@gmail.com>
2025-01-02 10:53:09 +0200

    Add os-browserify polyfill to craco configuration for compatibility

--- craco.config.js
+++ craco.config.js
@@ -6,6 +6,7 @@ module.exports = {
           fs: false, // Ignore fs
           path: require.resolve("path-browserify"), // Polyfill for path
           zlib: require.resolve("browserify-zlib"), // Polyfill for zlib
+          os: require.resolve("os-browserify/browser"), // Polyfill for os
         };
         return webpackConfig;
       },
